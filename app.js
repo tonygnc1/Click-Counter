ad8d366161b326fea61306157a34a9aed98206d6
tonygnc <bootcamp1955@gmail.com>
2019-12-30 15:30:37 -0500

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let counter = 0;
-document.addEventListener('DOMContentLoaded'), function(event) {
-    const body=document.querySelector('body');
-}
+document.addEventListener("DOMContentLoaded");
+  function(event) {
+    const body = document.querySelector("body");
+    let output = document.createElement('div');
+    body.appendChild(output);
+  };
